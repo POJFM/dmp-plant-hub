c7fb7119b943bd0b8d2d1b6131ffb8726e87827b
Filip Sikora <contact@fsikora.com>
2022-03-17 17:31:02 +0100

    nevim

--- scripts/postgres/test.sh
+++ scripts/postgres/test.sh
@@ -5,4 +5,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER docker;
     CREATE DATABASE docker;
     GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+    CREATE TABLE kokot(id int);
 EOSQL
