e1811b93b83d1405c6e8e0031cc8d58111a6451f
TurniXXD <legokubik@seznam.cz>
2022-03-26 20:39:10 +0100

    gql and db in test instance

--- test/go.mod
+++ test/go.mod
@@ -3,7 +3,31 @@ module github.com/SPSOAFM-IT18/dmp-plant-hub/test
 go 1.17
 
 require (
-	github.com/gorilla/mux v1.8.0
+	github.com/99designs/gqlgen v0.17.2
+	github.com/go-chi/chi v1.5.4
+	github.com/gorilla/websocket v1.4.2
 	github.com/jasonlvhit/gocron v0.0.1
 	github.com/joho/godotenv v1.4.0
+	github.com/rs/cors v1.8.2
+	github.com/uptrace/bun v1.1.2
+	github.com/uptrace/bun/dialect/pgdialect v1.1.2
+	github.com/uptrace/bun/driver/pgdriver v1.1.2
+	github.com/uptrace/bun/extra/bundebug v1.1.2
+	github.com/vektah/gqlparser/v2 v2.4.0
+)
+
+require (
+	github.com/agnivade/levenshtein v1.1.0 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/mapstructure v1.2.3 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064 // indirect
+	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
+	mellium.im/sasl v0.2.1 // indirect
 )
---
+++ test/graph/schema.resolvers.go
@@ -0,0 +1,46 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"log"
+
+	"github.com/SPSOAFM-IT18/dmp-plant-hub/test/graph/generated"
+	"github.com/SPSOAFM-IT18/dmp-plant-hub/test/graph/model"
+)
+
+func (r *mutationResolver) CreateMeasurement(ctx context.Context, input *model.NewMeasurement) (*model.Measurement, error) {
+	return r.DB.CreateMeasurement(ctx, input), nil
+}
+
+func (r *mutationResolver) CreateSettings(ctx context.Context, input *model.NewSettings) (*model.Setting, error) {
+	return r.DB.CreateSettings(ctx, input), nil
+}
+
+func (r *mutationResolver) UpdateSettings(ctx context.Context, input *model.NewSettings) (*model.Setting, error) {
+	return r.DB.UpdateSettings(ctx, input), nil
+}
+
+func (r *queryResolver) GetMeasurements(ctx context.Context) ([]*model.Measurement, error) {
+	return r.DB.GetMeasurements(ctx), nil
+}
+
+func (r *queryResolver) GetSettings(ctx context.Context) ([]*model.Setting, error) {
+	log.Println(ctx)
+	return r.DB.GetSettings(ctx), nil
+}
+
+func (r *queryResolver) GetIrrigation(ctx context.Context) ([]*model.IrrigationHistory, error) {
+	return r.DB.GetIrrigation(ctx), nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
