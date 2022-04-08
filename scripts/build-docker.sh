682a0f26cfefd1c36b0b1c16fb57d40bb96c3137
Filip Sikora <contact@fsikora.com>
2022-04-08 18:52:47 +0200

    docker pls work i beg you

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,5 +1,4 @@
 #/bin/bash
 
-cd ..
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t tassilobalbo/planthub-client --push client/.
 docker buildx build --platform linux/arm64,linux/arm/v7 -t tassilobalbo/planthub-server --push server/.
